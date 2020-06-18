83e8392bf484697ed6e5aa747a7ba2df30049f32
collerhatguy <jacobshomali3@gmail.com>
2020-06-18 07:50:58 -0400

    imported

---
+++ index.js
@@ -0,0 +1,15 @@
+function reveal(songName) {
+  var tab, tabcontent;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+
+  tab = document.getElementsByClassName("tab");
+  for (i = 0; i < tablinks.length; i++) {
+    tab[i].className = tab[i].className.replace("active", "");
+  }
+
+  document.getElementById(songName).style.display = "block";
+  this.className += "active";
+}
