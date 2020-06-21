a6b106d38f5c2de6e15d1e84f6656bab714dd368
collerhatguy <jacobshomali3@gmail.com>
2020-06-21 07:52:22 -0400

    meh
    i swapped arounf somelabels and numbers

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 function reveal(songName) {
   var tab, tabcontent;
-  tabcontent = document.getElementsByClassName("tabcontent");
+  tabcontent = document.querySelectorAll(".tabs .tab button");
   for (i = 0; i < tabcontent.length; i++) {
     tabcontent[i].style.display = "none";
   }
 
-  tab = document.getElementsByClassName("tab");
+  tab = document.querySelectorAll(".tabcontent");
   for (i = 0; i < tablinks.length; i++) {
     tab[i].className = tab[i].className.replace("active", "");
   }
