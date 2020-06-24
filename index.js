ce47f026ec13754ad28b266e23bbef779d3d3b53
collerhatguy <jacobshomali3@gmail.com>
2020-06-24 18:00:54 -0400

    created slider
    the slider  doesnt slide the audio files even though i set transitionn to 2 seconds. the select element works fine as well as the buttons.

--- index.js
+++ index.js
@@ -1,17 +1,30 @@
-const tabContents = document.querySelectorAll("[data-content]");
+const songSlide = document.querySelectorAll(".song-slide");
 var songNum = 0;
 function revolveR() {
-  if (songNum < tabContents.length) {
+  if (songNum < songSlide.length) {
     songNum++;
   }
   console.log(songNum);
+  move(songNum);
 }
 function revolveL() {
   if (songNum > 0) {
     songNum--;
   }
   console.log(songNum);
+  move(songNum);
 }
-function selectS() {
-  songNum = document.getElementById("#songChoice").nodeValue;
+function selectS(e) {
+  e.preventDefault();
+  var f = document.getElementById("songChoice");
+  songNum = f.value;
+  console.log(songNum);
+  move(songNum);
+}
+function move(num) {
+  songSlide.forEach((element) => {
+    element.className = "song-slide";
+  });
+  songSlide[num].className = "song-slide-main";
+  document.getElementById("sTitle").innerHTML = songSlide[num].id;
 }
