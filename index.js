a391d26d78fd1fd6461bf288e717368a090ac032
collerhatguy <jacobshomali3@gmail.com>
2020-06-25 11:11:31 -0400

    pause/play
    added a pause and play button

--- index.js
+++ index.js
@@ -15,16 +15,30 @@ function revolveL() {
   move(songNum);
 }
 function selectS(e) {
+  // stopps page from reseting//
   e.preventDefault();
+  // selects song choice and sets display equal to
   var f = document.getElementById("songChoice");
   songNum = f.value;
   console.log(songNum);
   move(songNum);
 }
 function move(num) {
+  // have every song set to hidden
   songSlide.forEach((element) => {
     element.className = "song-slide";
   });
+  // set chosen song to visible
   songSlide[num].className = "song-slide-main";
+
+  // set header equal to song name
   document.getElementById("sTitle").innerHTML = songSlide[num].id;
+}
+function playSong() {
+  var song = document.querySelector(".song-slide-main").children;
+  song[0].play();
+}
+function pauseSong() {
+  var song = document.querySelector(".song-slide-main").children;
+  song[0].pause();
 }
