b340d279d769c79245ace3413c442dbcf9fa65da
collerhatguy <67057792+collerhatguy@users.noreply.github.com>
2021-01-21 18:12:51 -0500

    button, naming, links
    rigged it so that buttons dissapear once the end of the list has been reached
    renamed variables for legibility
    fixed song links

--- index.js
+++ index.js
@@ -1,28 +1,37 @@
 const songSlide = document.querySelectorAll(".song-slide");
-var songNum = -1;
+var songNumber = 0;
 const songHeader = document.getElementById("songDisplay");
 const leftButton = document.getElementById("lButton");
 const rightButton = document.getElementById("rButton");
 const selectButton = document.getElementById("songChoice");
 
-leftButton.addEventListener("click", revolveL);
-rightButton.addEventListener("click", revolveR);
-selectButton.addEventListener("click", selectS);
-
-function revolveR() {
-  songNum < songSlide.length - 1 ? songNum++ : songNum = songNum;
+leftButton.addEventListener("click", revolveLeft);
+rightButton.addEventListener("click", revolveRight);
+selectButton.addEventListener("click", selectDirectly);
+// set the first song to being desplayed
+move(0);
+// set the left button to invisible;
+leftButton.style.display = "none";
+function revolveRight() {
+  if (songNumber >= songSlide.length - 2) rightButton.style.display = "none";
+  if (songNumber < songSlide.length - 1) songNumber++;
+  leftButton.style.display = "inline";
   pauseAll();
-  move(songNum);
+  move(songNumber);
 }
-function revolveL() {
-  songNum > 0 ? songNum-- : songNum = songNum;
+function revolveLeft() {
+  if (songNumber < 2) leftButton.style.display = "none";
+  if (songNumber > 0) songNumber--;
+  rightButton.style.display = "inline";
   pauseAll();
-  move(songNum);
+  move(songNumber);
 }
-function selectS() {
-  songNum = selectButton.value;
+function selectDirectly() {
+  if (songNumber < songSlide.length - 2) rightButton.style.display = "none";
+  if (songNumber <= 1 ) leftButton.style.display = "none";
+  songNumber = selectButton.value;
   pauseAll();
-  move(songNum);
+  move(songNumber);
 }
 function move(num) {
   // have every song set to hidden
